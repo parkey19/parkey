87e3bba30ff5332f1f5292bdbb425870cf661e68
SungJinPark <kkttqq@gmail.com>
2020-03-30 21:59:41 +0900

    dynamic dispatch anti pattern

--- src/main/java/com/reactive/parkey/Dispatch.java
+++ src/main/java/com/reactive/parkey/Dispatch.java
@@ -9,27 +9,44 @@ public class Dispatch {
     static class Text implements Post {
         @Override
         public void postOn(SNS sns) {
-            System.out.println("text ->" + sns.getClass().getSimpleName());
+            if (sns instanceof Facebook) {
+                System.out.println("text ->" + "facebook");
+            }
+            if (sns instanceof Twitter) {
+                System.out.println("text ->" + "twitter");
+            }
+            if (sns instanceof Googleplus) {
+                System.out.println("text ->" + "Googleplus");
+            }
         }
     }
 
     static class Picture implements Post {
         @Override
         public void postOn(SNS sns) {
-            System.out.println("picture ->" + sns.getClass().getSimpleName());
+            if (sns instanceof Facebook) {
+                System.out.println("picture ->" + "facebook");
+            }
+            if (sns instanceof Twitter) {
+                System.out.println("picture ->" + "twitter");
+            }
+            if (sns instanceof Googleplus) {
+                System.out.println("picture ->" + "googlePlus");
+            }
         }
     }
     
     interface SNS {}
     static class Facebook implements SNS {
-
     }
     static class Twitter implements SNS {
+    }
+    static class Googleplus implements SNS {
     }
 
     public static void main(String[] args) {
         List<Post> postList = Arrays.asList(new Text(), new Picture());
-        List<SNS> snsList = Arrays.asList(new Facebook(), new Twitter());
+        List<SNS> snsList = Arrays.asList(new Facebook(), new Twitter(), new Googleplus());
 
 
         //runtime 시점에 post에 어떤 클래스가 실행될지 결정 다이나믹 디스패치
