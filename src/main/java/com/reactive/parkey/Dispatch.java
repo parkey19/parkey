2a536b4e98ea62b838485160e600c16ba6467d1e
SungJinPark <kkttqq@gmail.com>
2020-03-30 22:10:06 +0900

    dynamic dispatch 메서드 오버로딩 사용 시 실패코드

--- src/main/java/com/reactive/parkey/Dispatch.java
+++ src/main/java/com/reactive/parkey/Dispatch.java
@@ -5,34 +5,29 @@ import java.util.List;
 
 public class Dispatch {
 
-    interface Post { void postOn(SNS sns);}
+    interface Post {
+        void postOn(Facebook sns);
+        void postOn(Twitter sns);
+    }
     static class Text implements Post {
         @Override
-        public void postOn(SNS sns) {
-            if (sns instanceof Facebook) {
-                System.out.println("text ->" + "facebook");
-            }
-            if (sns instanceof Twitter) {
-                System.out.println("text ->" + "twitter");
-            }
-            if (sns instanceof Googleplus) {
-                System.out.println("text ->" + "Googleplus");
-            }
+        public void postOn(Twitter sns) {
+            System.out.println("text ->" + "facebook");
+        }
+        @Override
+        public void postOn(Facebook sns) {
+            System.out.println("text ->" + "facebook");
         }
     }
 
     static class Picture implements Post {
         @Override
-        public void postOn(SNS sns) {
-            if (sns instanceof Facebook) {
-                System.out.println("picture ->" + "facebook");
-            }
-            if (sns instanceof Twitter) {
-                System.out.println("picture ->" + "twitter");
-            }
-            if (sns instanceof Googleplus) {
-                System.out.println("picture ->" + "googlePlus");
-            }
+        public void postOn(Facebook sns) {
+            System.out.println("picture ->" + "facebook");
+        }
+        @Override
+        public void postOn(Twitter sns) {
+            System.out.println("picture ->" + "facebook");
         }
     }
     
@@ -48,8 +43,9 @@ public class Dispatch {
         List<Post> postList = Arrays.asList(new Text(), new Picture());
         List<SNS> snsList = Arrays.asList(new Facebook(), new Twitter(), new Googleplus());
 
-
         //runtime 시점에 post에 어떤 클래스가 실행될지 결정 다이나믹 디스패치
+        //메서드 오버로딩은 컴파일 시점에 파라미터 타입이 정해져야한다
+        //현재 코드는 정적으로 정해지지 않고 런타임에 정해짐.
         postList.forEach(post -> snsList.forEach(sns -> post.postOn(sns)));
 
     }
