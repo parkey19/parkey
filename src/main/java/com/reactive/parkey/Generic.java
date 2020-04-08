b95e97434a87b45445750ba98d2f781cc2ae45fe
SungJinPark <kkttqq@gmail.com>
2020-04-08 23:46:08 +0900

    generic reverse 추가

--- src/main/java/com/reactive/parkey/Generic.java
+++ src/main/java/com/reactive/parkey/Generic.java
@@ -1,5 +1,6 @@
 package com.reactive.parkey;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,8 +9,12 @@ public class Generic {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 3, 2);
         System.out.println(max(list));
+//        reverse(list);
+//        reverse2(list);
+        reverse3(list);
+        System.out.println(list);
         System.out.println(Collections.max(list));
-        List<String> ns = Arrays.asList("1","2");
+        List<Integer> ns = Arrays.asList(1,2,3);
         System.out.println(getSize(ns));
     }
 
@@ -23,6 +28,33 @@ public class Generic {
      */
     private static <T extends Comparable<? super T>> T max(List<? extends T> list) {
         return list.stream().reduce((a, b) -> a.compareTo(b) > 0 ? a : b).get();
+    }
+
+    static <T> void reverse(List<T> list) {
+        List<T> temp = new ArrayList<>(list);
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, temp.get(list.size()-i-1));
+        }
+    }
+
+    static void reverse2(List<?> list) {
+        reverseHelper(list);
+    }
+
+    private static <T> void reverseHelper(List<T> list) {
+        List<T> temp = new ArrayList<>(list);
+        for (int i = 0; i < list.size(); i++) {
+            //capture 타입 추론하려고 하지만 타입 캡쳐불가
+            list.set(i, temp.get(list.size() - i - 1));
+        }
+    }
+
+    static void reverse3(List<?> list) {
+        List temp = new ArrayList<>(list);
+        List list2 = list;
+        for (int i = 0; i < list.size(); i++) {
+            list2.set(i, temp.get(list2.size()-i-1));
+        }
     }
 
     /**
